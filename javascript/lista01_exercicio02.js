f6e2790538a1f8b859b8820eb2c0bd0ba47e3a29
CarlosPhilipe <carlosphbahia@gmail.com>
2019-08-20 23:45:19 -0400

    Lista 01 exercicio 02 em javascript
    Teste de mesa

---
+++ javascript/lista01_exercicio02.js
@@ -0,0 +1,36 @@
+console.log(`Questao 02 item A`);
+var a1 = 10;
+var b1 = 20;
+console.log(b1);
+b1 = 5;
+console.log(a1, b1);
+
+console.log(`Questao 02 item B`);
+var a2 = 30;
+var b2 = 20;
+var c2 = a2 + b2;
+console.log(c2);
+b2 = 10;
+console.log(b2, c2);
+c2 = a2 + b2;
+console.log(a2, b2, c2);
+
+console.log(`Questao 02 item c`);
+
+var a3 = 10;
+var b3 = 20;
+var c3 = a3;
+b3 = c3;
+a3 = b3;
+console.log(a3, b3, c3);
+
+console.log(`Questao 02 item d`);
+var a4 = 10;
+var b4 = a4 + 1;
+a4 = b4 + 1;
+b4 = a4 + 1;
+console.log(a4);
+a4 = b4 + 1;
+console.log(a4, b4);
+
+console.log(`Questao 02 item e`);
